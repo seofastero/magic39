ed4fadda9e45bf847b23b56bd66c78997fae3058
seofastero <allintv14@gmail.com>
2024-01-15 07:17:55 +0200

    Update contacts.ts

--- src/shared/Mocks/contacts.ts
+++ src/shared/Mocks/contacts.ts
@@ -1,5 +1,5 @@
 export const contactsList = {
-    'whatsapp': 'https://wa.me/79998988988?text=Здравствуйте%2C+я+нашел+контакт+вашего+салона+Magic+на+сайте.+Хотел+бы+узнать%2C+кто+из+мастеров+сегодня+на+смене+и+на+какое+время+можно+записаться+на+сеанс.',
+    'whatsapp': 'https://wa.me/79255557799?text=Здравствуйте%2C+я+нашел+контакт+вашего+салона+Magic+на+сайте.+Хотел+бы+узнать%2C+кто+из+мастеров+сегодня+на+смене+и+на+какое+время+можно+записаться+на+сеанс.',
     'telegram': 'https://t.me/magic039',
     'instagram': 'https://instagram.com/_magic_39?igshid=OGQ5ZDc2ODk2ZA==',
     'geolocation': 'https://yandex.ru/maps/-/CDqjaL~b',
